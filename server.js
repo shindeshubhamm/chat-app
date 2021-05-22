584812fa9d352e0c34f99e096a08443a06d65fe4
Shubham Shinde <shubhamshinde.3151@gmail.com>
2021-05-22 11:32:16 +0530

    Project initialized.

---
+++ server.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const http = require('http');
+const express = require('express/');
+const socketio = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+io.on('connection', (socket) => {
+    console.log('socket');
+});
+
+server.listen(PORT, () => {
+    console.log(`> Server running on: ${PORT}`);
+});
